a160af8e5666cd0e79158be150a802e90e4441c2
Tim Zimmermann <tim@linux4.de>
2020-09-11 10:43:35 +0200

    gta4xl-common: add support for gta4xl (LTE variant)
    Change-Id: Ie2d0078353ed6dcf0c51d7e46dc0802ba437d53b

--- Android.mk
+++ Android.mk
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter gta4xlwifi gtax4llte, $(TARGET_DEVICE)),)
+ifneq ($(filter gta4xlwifi gta4xl, $(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -39,7 +39,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "gta4xlwifi gta4xllte"
+write_headers "gta4xlwifi gta4xl"
 
 write_makefiles "$MY_DIR"/proprietary-files.txt true
 write_makefiles "$MY_DIR"/proprietary-files-vendor.txt true
