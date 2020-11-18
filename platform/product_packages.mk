27716fd3fb2cdb73abe91dfda4ee9f3bbcdbb22f
Tim Zimmermann <tim@linux4.de>
2020-11-18 12:53:30 +0100

    gta4xl-common: downgrade soundtrigger hal to 2.0
    * Stock blobs don't seem to support > 2.0
    
    Change-Id: I371ab7f7617235a695f1b47b691a88c1467f7bdb

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -145,8 +145,7 @@ PRODUCT_PACKAGES += \
 
 ### SOUNDTRIGGER
 PRODUCT_PACKAGES += \
-    android.hardware.soundtrigger@2.2-impl \
-    android.hardware.soundtrigger@2.0-service
+    android.hardware.soundtrigger@2.0-impl
 
 ### THERMAL
 PRODUCT_PACKAGES += \
