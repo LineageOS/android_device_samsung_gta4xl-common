f6850b3dc88ff04e55f7727d948f6901d66c71f8
Tim Zimmermann <tim@linux4.de>
2021-01-02 16:01:53 +0100

    gta4xl-common: build camera provider from source
    Change-Id: I0cf78b60b06f1ee5e7755e8dda0350e24e0d1fc2

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -151,7 +151,8 @@ TARGET_KEYMASTER_VARIANT := samsung
 ### SHIMS
 TARGET_LD_SHIM_LIBS += \
     /vendor/lib/vndk/libstagefright_omx_utils.so|libshim_stagefright_foundation.so \
-    /vendor/lib/libsensorlistener.so|libshim_sensorndkbridge.so
+    /vendor/lib/libsensorlistener.so|libshim_sensorndkbridge.so \
+    /vendor/lib64/libsensorlistener.so|libshim_sensorndkbridge.so
 
 ### SEPOLICY
 include device/lineage/sepolicy/exynos/sepolicy.mk
