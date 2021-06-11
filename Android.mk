4e4bed8db51751c89973a6537150cd4aae467010
Tim Zimmermann <tim@linux4.de>
2021-06-11 14:31:40 +0200

    gta4xl-common: align to standard devicetree structure
    Change-Id: Ie09b02b415895143ffed7451bef1dd863eca3145

--- Android.mk
+++ Android.mk
@@ -19,8 +19,6 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter gta4xlwifi gta4xl, $(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-include $(COMMON_PATH)/AndroidBoard.mk
-
 include $(CLEAR_VARS)
 
 EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 hw/vulkan.universal9611.so
--- README.md
+++
@@ -1,5 +0,0 @@
-Copyright 2020 - The LineageOS Project  
-  
-LineageOS platform tree for Samsung Galaxy Tab S6 Lite WiFi & LTE  
-  
-Thanks to all the people who made the sources for [exynos9820-common](https://github.com/whatawurst/android_device_samsung_exynos9820-common) on which this tree is heavily based  
