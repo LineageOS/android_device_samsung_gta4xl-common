b73aaf6bf71729892d0d13dbe5faa18be9826f77
Tim Zimmermann <tim@linux4.de>
2021-06-13 07:12:02 +0200

    gta4xl-common: cleanup init scripts
    * Also rename init.exynos9611.root.rc to init.samsung.rc
    
    Change-Id: I3087f71b17924188f421c91b90804c5133a9e48b

--- common.mk
+++ common.mk
@@ -115,9 +115,9 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/fstab.exynos9611:$(TARGET_COPY_OUT_RAMDISK)/fstab.exynos9611 \
     $(COMMON_PATH)/configs/init/fstab.exynos9611:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.exynos9611 \
     $(COMMON_PATH)/configs/init/init.exynos9611.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.exynos9611.rc \
-    $(COMMON_PATH)/configs/init/init.exynos9611.root.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.exynos9611.root.rc \
     $(COMMON_PATH)/configs/init/init.exynos9611.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.exynos9611.usb.rc \
     $(COMMON_PATH)/configs/init/init.recovery.exynos9611.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.exynos9611.rc \
+    $(COMMON_PATH)/configs/init/init.samsung.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsung.rc \
     $(COMMON_PATH)/configs/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
 
 # Keylayout
