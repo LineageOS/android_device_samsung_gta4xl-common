d7e1c6eb440b48df8af64d3fcdf268bf93881e9b
Tim Zimmermann <tim@linux4.de>
2021-11-21 08:57:14 +0100

    gta4xl-common: switch to new AIDL light HAL
    Change-Id: I7917b1da162e28187216fc8f55f164fae2ec8eef

--- common.mk
+++ common.mk
@@ -140,7 +140,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.samsung
+    android.hardware.light-service.samsung
 
 # Media
 PRODUCT_COPY_FILES += \
