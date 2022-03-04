840ee835e362b73252a14d16aae94147ed31a24e
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:27:02 +0000

    gta4xl-common: Removed audio A2dp and Hearing Aid from device.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: Ia1d4fbe0e406e895d295936c524c5dbba6c269cd

--- common.mk
+++ common.mk
@@ -27,7 +27,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl:32 \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
