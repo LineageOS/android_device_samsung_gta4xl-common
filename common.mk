8f68ac5373ab37d2a73cc0905b1dfcc012714a0c
Tim Zimmermann <tim@linux4.de>
2022-08-17 18:08:25 +0200

    gta4xl-common: Use libutils from VNDK32 for HWC and sensors
    * For sensors.sensorhub.so and libexynosdisplay.so
    * Abort message: 'incStrongRequireStrong() called on 0x7586a3a1c0 which isn't already owned'
    * Unfortunately the HIDL service loads libutils.so too, so that needs to be patched instead
    
    Change-Id: Ibca93bc356b81e205c77df5992b2ae1ded9bb83b

--- common.mk
+++ common.mk
@@ -257,6 +257,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.samsung
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libutils-v32
+
 # WiFi
 PRODUCT_PACKAGES += \
     WifiOverlay \
