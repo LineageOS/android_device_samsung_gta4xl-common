70271cfd1cc9c30257a628d8b866f4a8bcb075f2
Tim Zimmermann <tim@linux4.de>
2022-10-07 16:17:16 +0200

    gta4xl-common: Support updating firmware
    Change-Id: Iff87746b6e3efd5ed0af207e689fd068ad965212

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -34,9 +34,53 @@ def AddImage(info, basename, dest):
 def PrintInfo(info, dest):
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
 
+def AddFirmwareImage(info, model, basename, dest, simple=False, offset=8):
+  if ("RADIO/%s_%s" % (basename, model)) in info.input_zip.namelist():
+    data = info.input_zip.read("RADIO/%s_%s" % (basename, model))
+    common.ZipWriteStr(info.output_zip, "firmware/%s/%s" % (model, basename), data);
+    info.script.Print("Patching {} image unconditionally...".format(basename.split('.')[0]));
+    if simple:
+      info.script.AppendExtra('package_extract_file("firmware/%s/%s", "%s");' % (model, basename, dest))
+    else:
+      size = info.input_zip.getinfo("RADIO/%s_%s" % (basename, model)).file_size
+      info.script.AppendExtra('assert(exynos9611.write_data_bt("firmware/%s/%s", "%s", %d, %d));' % (model, basename, dest, offset, size))
+      return size
+    return 0
+
 def OTA_InstallEnd(info):
   PrintInfo(info, "/dev/block/by-name/dtbo")
   AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
   PrintInfo(info, "/dev/block/by-name/vbmeta")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
+
+  if "RADIO/models" in info.input_zip.namelist():
+    modelsIncluded = []
+    for model in info.input_zip.read("RADIO/models").decode('utf-8').splitlines():
+      if "RADIO/version_%s" % model in info.input_zip.namelist():
+        modelsIncluded.append(model)
+        version = info.input_zip.read("RADIO/version_%s" % model).decode('utf-8').splitlines()[0]
+        offset = 8
+        numImages = 0
+        info.script.AppendExtra('# Firmware update to %s for %s' % (version, model))
+        info.script.AppendExtra('ifelse (getprop("ro.boot.em.model") == "%s" &&' % model)
+        info.script.AppendExtra('exynos9611.verify_no_downgrade("%s") == "0" &&' % version)
+        info.script.AppendExtra('getprop("ro.boot.bootloader") != "%s",' % version)
+        info.script.AppendExtra('assert(exynos9611.mark_header_bt("/dev/block/by-name/bota", 0, 0, 0));')
+        for image in 'cm.bin', 'keystorage.bin', 'sboot.bin', 'uh.bin', 'up_param.bin':
+          size = AddFirmwareImage(info, model, image, "/dev/block/by-name/bota", False, offset)
+          if size > 0:
+            numImages += 1
+            offset += size + 36 # header size
+        info.script.AppendExtra('assert(exynos9611.mark_header_bt("/dev/block/by-name/bota", 0, %d, 3142939818));' % numImages)
+        AddFirmwareImage(info, model, "modem.bin", "/dev/block/by-name/radio", True)
+        AddFirmwareImage(info, model, "modem_debug.bin", "/dev/block/by-name/cp_debug", True)
+        info.script.AppendExtra(',"");')
+
+    modelCheck = ""
+    for model in modelsIncluded:
+      if len(modelCheck) > 0:
+        modelCheck += ' || '
+      modelCheck += 'getprop("ro.boot.em.model") == "%s"' % model
+    if len(modelCheck) > 0:
+      info.script.AppendExtra('%s || abort("Unsupported model, not updating firmware!");' % modelCheck)
   return
