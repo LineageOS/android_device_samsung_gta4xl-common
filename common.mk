9afb1f6a2a0ab6c0cd21354b18d9fc6b1629290f
LuK1337 <priv.luk@gmail.com>
2022-11-03 20:57:11 +0100

    gta4xl-common: Add hardware/samsung to soong namespaces
    See I78b1b9930a57bc01afda722b9f8a196c372f9d8b.
    
    Change-Id: I634b562e12b70a0c060d449d3ba1d4e87204cf5d

--- common.mk
+++ common.mk
@@ -241,6 +241,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/samsung \
     hardware/samsung/aidl/power-libperfmgr
 
 # Thermal
