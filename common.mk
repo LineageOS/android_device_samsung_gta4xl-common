9657d85dbba2c65fe55ecbcc41302ab5e5d8d3dd
Jan Altensen <info@stricted.net>
2025-01-27 22:04:12 +0100

    gta4xl-common: Include gralloc3 namespace
    Change-Id: I6649654bf3302a560e912402949c5e4a71c83d64

--- common.mk
+++ common.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.composer.hwc3-service.slsi \
-    gralloc.exynos9611 \
+    gralloc.universal9611 \
     libion
 
 PRODUCT_PACKAGES += \
@@ -261,6 +261,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/samsung \
+    hardware/samsung_slsi-linaro/exynos/gralloc/gralloc3 \
     hardware/samsung_slsi/libbt
 
 # Thermal
