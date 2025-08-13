3a6395959b644bfb0be8aab93982f5d2cc17c691
LuK1337 <priv.luk@gmail.com>
2025-08-13 21:56:50 +0200

    gta4xl-common: Migrate to lineagehealth IFastCharge
    Change-Id: Ia2f1f213de2f2e5afd913e5de38712fddc386b49

--- common.mk
+++ common.mk
@@ -99,10 +99,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# FastCharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.samsung
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
@@ -143,6 +139,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-service.samsung
 
 # Lineage Health
+$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/sec/switch/afc_disable)
+$(call soong_config_set,lineage_health,fast_charge_value_none,1)
+$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
+
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
