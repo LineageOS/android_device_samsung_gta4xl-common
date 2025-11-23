da5f34e9a04469779588bc4bc5e7b31d98dd6296
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    gta4xl-common: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,7 +59,7 @@ BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_TAGS_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
 ## Camera
-$(call soong_config_set,samsungCameraVars,usage_64bit,true)
+$(call soong_config_set_bool,samsungCameraVars,usage_64bit,true)
 
 ## Display
 BOARD_MINIMUM_DISPLAY_BRIGHTNESS := 1
