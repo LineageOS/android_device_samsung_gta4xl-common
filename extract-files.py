b6b01790445ffe44a9f874ea57e78f3e1033e9dd
Tim Zimmermann <tim@linux4.de>
2026-01-23 21:31:35 +0100

    gta4xl-common: Add dummy shared lib for libexynoscamera3
    Change-Id: I863c9ba1db58106e31d77b359655d9c46fc2b960

--- extract-files.py
+++ extract-files.py
@@ -26,12 +26,8 @@ namespace_imports = [
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
 
-def lib_fixup_device_dep(lib: str, partition: str, *args, **kwargs):
-    return f'//device/samsung/gta4xl-common/shims/stub:{lib}'
-
 lib_fixups: lib_fixups_user_type = {
     libs_proto_3_9_1: lib_fixup_vendorcompat,
-    'libexynoscamera3': lib_fixup_device_dep,
     'libuuid': lib_fixup_vendor_suffix,
 } # fmt: skip
 
